8cf36121525388438be24179dd46764fdd0e6baf
Duncan McDougall <duncanmcdougall@duncanmgallsmbp.home>
2015-10-02 15:59:50 +0100

    Various fixes after latest pull request

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,57 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    uglify: {
-      build: {
-        src: 'jquery.lightbox.js',
-        dest: 'jquery.lightbox.min.js'
-      },
-      options: {
-        report : 'gzip',
-        preserveComments : 'some'
-      }
-    },
+        uglify: {
+            build: {
+                src: 'jquery.lightbox.js',
+                dest: 'jquery.lightbox.min.js'
+            },
+            options: {
+                report: 'gzip',
+                preserveComments: 'some'
+            }
+        },
 
-    cssmin: {
-      compress: {
-        files: {
-          'jquery.lightbox.min.css': ['jquery.lightbox.css']
+        cssmin: {
+            compress: {
+                files: {
+                    'jquery.lightbox.min.css': ['jquery.lightbox.css']
+                }
+            },
+            options: {
+                report: 'gzip'
+            }
+        },
+
+        jshint: {
+            all: ['jquery.lightbox.js']
+        },
+
+        copy: {
+            build: {
+                files: [{
+                    src: 'jquery.lightbox.css',
+                    dest: 'scss/jquery.lightbox.scss'
+                }, 
+                {
+                    src: 'jquery.lightbox.css',
+                    dest: 'less/jquery.lightbox.less'
+                }]
+            }
         }
-      },
-      options: {
-        report : 'gzip'
-      }
-    },
+    });
 
-    jshint: {
-      all: ['jquery.lightbox.js']
-    }
-  });
+    // Load the plugin that provides the "uglify" task.
 
-  // Load the plugin that provides the "uglify" task.
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify', 'cssmin', 'jshint']);
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'cssmin', 'jshint', 'copy']);
 
 };
